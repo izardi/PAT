2a34b114861826db5bc0ea113af976a0464c162e
yuhao <yuhaomkp@163.com>
2017-10-21 18:33:09 +0800

    yes

--- 1017. A除以B.c
+++ 1017. A除以B.c
@@ -1,46 +1,37 @@
 #include <stdio.h>
-int main(){
-    int A[1001], B;
-    int C[1001];
-    char c;
+int main() {
+    char A[1001];
+    int B;
     int i = 0;
-    scanf("%c", &c);
-    while (c != ' '){
-        A[i++] = c - '0';
-        scanf("%c", &c);
+    int tmp =0;
+    scanf("%c", &A[i++]);
+    while(A[i-1] != ' '){
+        scanf("%c", &A[i++]);
     }
-    //array length = i-1
     scanf("%d", &B);
-    if(B != 1){
-        int tmp, k=1;
-        if (A[0] >= B){
-            C[k-1] = A[0]/B;
-            tmp = A[0]%B;
-            for(k=1; k<i; k++){
-                tmp = tmp*10 + A[k];
-                C[k] = tmp/B;
-                tmp = tmp%B;
-            }
-        }else {
-            tmp = (A[k-1]*10+A[k]);
-            C[k-1] = tmp/B;
-            tmp = tmp % B;
-            for(k=2; k<i; k++){
-                tmp = tmp*10 + A[k];
-                C[k-1] = tmp/B;
-                tmp = tmp%B;
+    //read end
+    if(A[1] ==' '){
+        if(A[0]-'0' >= B){
+            printf("%d %d", (A[0]-'0')/B, (A[0]-'0')%B);
+        }else{
+            printf("0 %d", (A[0]-'0')%B);
+        }
+    }else {
+        for (i = 0; A[i] != ' '; i++) {
+            if (i == 0) {
+                if ((A[i] - '0') > B) {
+                    printf("%d", (A[i] - '0') / B);
+                    tmp = (A[i] - '0') % B;
+                } else {
+                    tmp = A[i] - '0';
+                }
+            } else {
+                tmp = tmp * 10 + (A[i] - '0');
+                printf("%d", tmp / B);
+                tmp = tmp % B;
             }
         }
-        for(int j=0; j<k-1; j++){
-            printf("%d",C[j]);
-        }
-        printf(" ");
-        printf("%d", tmp);
-    }
-    else {
-        for(int j = 0; j < i; j++)
-            printf("%d", A[j]);
-        printf(" 0");
+        printf(" %d", tmp);
     }
     return 0;
 }
