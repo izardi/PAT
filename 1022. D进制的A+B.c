bcd2a11ae1c8d16d2c77bed44d95857a92aeae11
yuhao <yuhaomkp@163.com>
2017-10-27 18:42:47 +0800

    YES

--- 1022. D进制的A+B.c
+++ 1022. D进制的A+B.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 int main(){
     int A, B, C, D;
-    int array[32];
+    int array[31];
     int i = 0;
     scanf("%d %d %d", &A, &B, &D);
     C = A + B;
-    int k;
-    if( C > D) {
-        do {
-            k = C % D;
-            array[i++] = k;
-            C = C / D;
-        } while (C > D);
-        array[i] = C;
-        for(int k=i; k >=0; k--){
-            printf("%d",array[k]);
+    if(C != 0 ) {
+        while (C != 0) {
+            array[i++] = C % D;
+            C /= D;
         }
-    }else {
-        printf("%d" , C);
-    }
+        for (int k = i - 1; k >= 0; k--)
+            printf("%d", array[k]);
+    }else
+        printf("0");
     return 0;
 }
