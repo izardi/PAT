2c7acc90364030ad3b62a6bf8fdad4d8ef789c6d
yuhh-cs <yuhh.cs@gmail.com>
2018-12-08 13:43:37 +0800

    AC

---
+++ PAT (Advanced Level) Practice/1094 The Largest Generation （25 point(s)）.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+	int N, M;
+	scanf("%d %d", &N, &M);
+
+	int id, num, child;
+	vector<int> Mb[100];
+
+	for(int i = 0; i < M; ++i){
+		scanf("%d %d", &id, &num);
+		for(int j = 0; j < num; ++j){
+			scanf("%d", &child);
+			Mb[id].push_back(child);
+		}
+	}
+	
+	queue<int> que;
+	que.push(1);
+
+	int level = 1, curLevel=1, maxLevel=0, curP=0, maxP=0;
+	int last = 1, levelLast = 1;
+
+	while(!que.empty()){
+		int cur = que.front();
+		que.pop();
+		++curP;
+		for(vector<int>::iterator it = Mb[cur].begin(); it != Mb[cur].end(); ++it){
+			que.push(*it);
+			last = *it;
+		}
+		if(cur == levelLast){
+			levelLast = last;
+			if(maxP < curP){
+				maxP = curP;
+				level = curLevel;
+			}
+			curP = 0;
+			++curLevel;
+		}
+	}
+
+	printf("%d %d", maxP, level);
+	return 0;
+}
