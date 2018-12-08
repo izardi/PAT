1c0509b914d1c6903e4a759c09f877ac23ed14f6
yuhh-cs <yuhh.cs@gmail.com>
2018-12-08 15:27:05 +0800

    AC

---
+++ PAT (Advanced Level) Practice/1090 Highest Price in Supply Chain （25 point(s)）.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+	int N;
+	double P, r;
+	scanf("%d %lf %lf", &N, &P, &r);
+	r /= 100;
+	
+	int tmp;
+	vector<int> V[100005];
+	int root;
+
+	for(int i = 0; i < N; ++i){
+		scanf("%d", &tmp);
+		if(tmp != -1)
+			V[tmp].push_back(i);
+		else
+			root = i;
+	}
+
+	queue<int> que;
+	que.push(root);
+
+	int cur, level = 0, levelNum = 0, num = 0;
+	int last = 0, levelLast = root;
+
+	while(!que.empty()){
+		cur = que.front();
+		que.pop();
+		++levelNum;
+		for(vector<int>::iterator it = V[cur].begin(); it != V[cur].end(); ++it){
+			que.push(*it);
+			last = *it;
+		}
+
+		if(cur == levelLast){
+			++level;
+			levelLast = last;
+			num = levelNum;
+			levelNum = 0;
+		}
+	}
+
+	double res = P;
+	for(int i = 1; i < level; ++i)
+		res *= (1+r);
+
+	printf("%.2lf %d", res, num);
+	return 0;
+}
