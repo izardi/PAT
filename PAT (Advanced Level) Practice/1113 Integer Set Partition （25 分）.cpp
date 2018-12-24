9d81258ea02e7bdedcc19d06a292180a19e9d7fa
yuhh-cs <yuhh.cs@gmail.com>
2018-12-24 10:08:28 +0800

    AC

---
+++ PAT (Advanced Level) Practice/1113 Integer Set Partition （25 分）.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	freopen("data.in", "r", stdin);
+	int N, data[1005] = {0};
+	scanf("%d", &N);
+	int sum = 0;
+
+	for(int i = 0; i < N; ++i){
+		scanf("%d", &data[i]);
+		sum += data[i];
+	}
+
+	sort(data, data + N);
+	int halfSum = 0;
+
+	for(int i = 0; i < N / 2; ++i)
+		halfSum += data[i];
+
+	printf("%d %d\n", N % 2, sum - 2*halfSum);
+	return 0;
+}
