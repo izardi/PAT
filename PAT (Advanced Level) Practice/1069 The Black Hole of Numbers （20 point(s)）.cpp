33e30a6bd9291a7b79484256a36692863b20d1de
yh-cs <yuhh.cs@gmail.com>
2019-01-06 10:56:40 +0800

    AC

---
+++ PAT (Advanced Level) Practice/1069 The Black Hole of Numbers （20 point(s)）.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool cmp(const char &a, const char &b){
+	return a > b;
+}
+int main(){
+	// freopen("data.in", "r", stdin);
+	int res;
+	string s, a, b;
+	cin >> s;
+	s.insert(0, 4-s.length(), '0');
+	do{
+		a = s; b =s;
+		sort(a.begin(), a.end(), cmp);
+		sort(b.begin(), b.end());
+		res = stoi(a) - stoi(b);
+		s = to_string(res);
+		s.insert(0, 4-s.length(), '0');
+		cout << a << " - " << b << " = " << s << endl;
+	}while(s != "6174" && s != "0000");
+	return 0;
+}
