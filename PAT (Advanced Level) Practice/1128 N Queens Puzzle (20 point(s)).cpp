6d18e42ced616270a6305f29e4a0530c428b7396
yh-cs <yuhh.cs@gmail.com>
2019-06-15 20:25:15 +0800

    AC

---
+++ PAT (Advanced Level) Practice/1128 N Queens Puzzle (20 point(s)).cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main(){
+	//freopen("d.txt", "r", stdin);
+	int N, K, tmp;
+	scanf("%d", &K);
+
+	while(K--){
+		int chessborad[1005] = {0};
+		scanf("%d", &N);
+		bool res = true;
+		for(int i = 1; i <= N; ++i){
+			scanf("%d", &chessborad[i]);
+			for(int j = 1; j < i; ++j)
+				if(chessborad[i] == chessborad[j] || (i - j) == abs(chessborad[i] - chessborad[j])){
+					res = false;
+					break;
+				}
+		}
+		printf("%s\n", res == false ? "NO" : "YES");
+	}
+	return 0;
+}
